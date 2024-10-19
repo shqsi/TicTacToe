c2f88e3c427a65f6a7ba809bc4a2f46c2793d150
shqsi <elshannazri8@gmail.com>
2024-10-19 09:50:08 +0000

    Added about section

--- js/game.js
+++ js/game.js
@@ -1,11 +1,21 @@
 
 $(document).ready(function () {
-    [$('#header-winner-x'), $('#header-winner-o'), $('.grid')].forEach(header => {
+    [$('#header-winner-x'), $('#header-winner-o'), $('.grid'), $('#about-ttt-text')].forEach(header => {
         header.hide();
     });
 
     $('#header-welcome').show(1000);
     $('.grid').show(1000);
 
+    $('#about-ttt-dropdown-text-inner').hover(
+        // mouse enter
+        function(){
+            $('#about-ttt-text').fadeIn('slow');
+        },
 
+        // mouse leave
+        function(){
+            $('#about-ttt-text').fadeOut('slow');
+        }
+    )
 })
