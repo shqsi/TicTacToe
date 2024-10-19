57c920f583cfd5f7bb48f1938b80c4fb65d0d303
shqsi <elshannazri8@gmail.com>
2024-10-19 10:38:29 +0000

    Initial

--- js/game.js
+++ js/game.js
@@ -1,21 +1,13 @@
-
-$(document).ready(function () {
-    [$('#header-winner-x'), $('#header-winner-o'), $('.grid'), $('#about-ttt-text')].forEach(header => {
-        header.hide();
+function game() {
+    $('.game-cell').forEach(cell => {
+        cell.click(function() {
+            cellClicked(cell);
+        });
     });
+}
 
-    $('#header-welcome').show(1000);
-    $('.grid').show(1000);
+let player = 'X';
 
-    $('#about-ttt-dropdown-text-inner').hover(
-        // mouse enter
-        function(){
-            $('#about-ttt-text').fadeIn('slow');
-        },
-
-        // mouse leave
-        function(){
-            $('#about-ttt-text').fadeOut('slow');
-        }
-    )
-})
+function cellClicked(cell) {
+    
+}
